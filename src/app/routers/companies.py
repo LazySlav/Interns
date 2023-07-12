83fb6cde271f81b2a36a402c57276918eee1e717
LazySlav <72216859+LazySlav@users.noreply.github.com>
2023-07-12 12:55:02 +0300

    ANOTHER one router example

--- src/app/routers/companies.py
+++ src/app/routers/companies.py
@@ -1,12 +1,18 @@
-from typing import List
 from uuid import UUID
 from fastapi import APIRouter
 from app.models import CompanyModel
 from app.schemas import CompanySchema
 from app.crud import *
 from app.main import ASYNC_SESSIONMAKER
+
+
 router = APIRouter()
 
-@router.get("/{id}",response_model=CompanySchema)
+
+@router.get("/{id}",response_model=CompanyModel)
 async def get_company(id : UUID):
   return await read_entity(ASYNC_SESSIONMAKER, CompanyModel, id=id)
+
+@router.post("/",response_model=CompanyModel)
+async def post_company(payload: CompanySchema):
+  return await create_entity(ASYNC_SESSIONMAKER, CompanyModel, payload)
