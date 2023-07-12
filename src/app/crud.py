dc20198e9792f277afad5c521335d0278dcc9756
LazySlav <72216859+LazySlav@users.noreply.github.com>
2023-07-12 12:02:00 +0300

    Pre-final crud

--- src/app/crud.py
+++ src/app/crud.py
@@ -1,29 +1,17 @@
-from uuid import UUID, uuid4
 from pydantic import BaseModel as BaseSchema
-from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
-from sqlalchemy import select, insert, update
-from app.models import CompanyModel
+from sqlalchemy import select, update, delete
 from typing import List
-from fastapi import APIRouter, HTTPException, Path
-from app.schemas import CompanySchema
+from fastapi import APIRouter
 router = APIRouter()
 
-def __get_model_name(obj : object):
-    return obj.__class__
 
-# def __get_model_attrs(obj : DeclarativeBase):
-#     return {i:k for i,k in vars(obj).items() if i[:2]!="__" and i[0]!="_"}
-
-# ************* Generic Test Methods *******************
-# @router.get("/",status_code=201)
 async def read_all_entities(async_session: async_sessionmaker[AsyncSession], model_type):
     async with async_session() as session:
         async with session.begin():
             statement = select(model_type).all()
             return await session.scalars(statement).all()
 
-# @router.get("/{id}",status_code=201)
 async def read_entity(async_session: async_sessionmaker[AsyncSession], model_type, **kwargs):
     async with async_session() as session:
         async with session.begin():
@@ -36,37 +24,34 @@ async def read_entity(async_session: async_sessionmaker[AsyncSession], model_typ
 #             statement = select(model_type).
 #             return await session.scalars(statement).all()
 
-# @router.post("/",status_code=201)
 async def create_entity(async_session: async_sessionmaker[AsyncSession], model_type, model: BaseSchema):
     async with async_session() as session:
         async with session.begin():
             entity = model_type(**model.model_dump())
             await session.add(entity)
+            return entity
 
 async def create_entity_list(async_session: async_sessionmaker[AsyncSession], model_type, model_list: List[BaseSchema]):
     async with async_session() as session:
         async with session.begin():
             entity_list = [model_type(**model.model_dump()) for model in model_list]
             await session.add_all(entity_list)
+            return entity_list
 
-# @router.put("/{id}",status_code=201)
-async def update_entity(async_session: async_sessionmaker[AsyncSession], id: UUID, model_type, model: BaseSchema):
+async def update_entity(async_session: async_sessionmaker[AsyncSession], model_type, model: BaseSchema):
     async with async_session() as session:
         async with session.begin():
-            query    = model_type(**model.model_dump())
+            entity_dict = model.model_dump()
+            entity = model_type(**entity_dict)
+            query = update(model_type,[entity_dict])
             await session.execute(query)
+            return entity
 
-
-# @router.get("/{id}",status_code=201, response_model=CompanySchema)
-# async def read_company(async_session: async_sessionmaker[AsyncSession], id: UUID):
-#     async with async_session() as session:
-#         async with session.begin():
-#             query = select(CompanyModel).where(CompanyModel.id == id)
-#             return await session.execute(query)
-
-# @router.post("/",status_code=201,response_model=CompanySchema)
-# async def create_company(async_session: async_sessionmaker[AsyncSession], company: CompanySchema):
-#     async with async_session() as session:
-#         async with session.begin():
-#             db_user = CompanySchema(id=id,)
-#             await session.add(db_user)
+async def delete_entity(async_session: async_sessionmaker[AsyncSession], model_type, model: BaseSchema):
+    async with async_session() as session:
+        async with session.begin():
+            entity_dict = model.model_dump()
+            entity = model_type(**entity_dict)
+            query = delete(model_type,[entity_dict])
+            await session.execute(query)
+            return entity
