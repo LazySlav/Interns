3738a2e2381dad636f6f873f260eedf74810b8f9
LazySlav <72216859+LazySlav@users.noreply.github.com>
2023-07-12 18:38:47 +0300

    main.py changes

--- src/app/main.py
+++ src/app/main.py
@@ -4,12 +4,13 @@ This file is for connecting routers and creating DB engine
 
 
 from fastapi import FastAPI
+import os
+from app.models import Base
+from sqlalchemy.ext.asyncio import async_sessionmaker,create_async_engine
+from fastapi import FastAPI
 from fastapi_async_sqlalchemy import SQLAlchemyMiddleware
-
-from app.database import engine
-# from app.routers import companies,curators,mentors,students
-# metadata.create_all(engine)
-
+from app.routers import companies,curators,mentors,students,users
+ 
 app = FastAPI()
 app.add_middleware(
     SQLAlchemyMiddleware,
@@ -25,7 +26,23 @@ app.add_middleware(
         },
     },
 )
-#app.include_router(companies.router, prefix="/companies")
-# app.include_router(curators.router, prefix="/curators")
-# app.include_router(mentors.router, prefix="/mentors")
-# app.include_router(students.router, prefix="/students") 
+app.include_router(companies.router)
+app.include_router(curators.router)
+app.include_router(mentors.router)
+app.include_router(students.router)
+app.include_router(users.router)
+
+##### TODO: migrate all of sqlalchemy initial start stuff into separate file #####
+DATABASE_URL = os.getenv("DATABASE_URL")
+ENGINE = create_async_engine(DATABASE_URL)
+with ENGINE.begin() as conn:
+    conn.run_sync(Base.metadata.create_all)
+
+# ? the fuck is this
+# for AsyncEngine created in function scope, close and
+# clean-up pooled connections
+# await ENGINE.dispose()
+#     
+ASYNC_SESSIONMAKER = async_sessionmaker(ENGINE, expire_on_commit=False) # expire_on_commit - don't expire objects after transaction commit
+
+##############################################################################
