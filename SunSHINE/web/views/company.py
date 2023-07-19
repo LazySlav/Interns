9a811a7c99feaa0f86852accf1a79a619828553b
LazySlav <72216859+LazySlav@users.noreply.github.com>
2023-07-19 17:42:19 +0300

    Possibly working crud draft

--- SunSHINE/web/views/company.py
+++ SunSHINE/web/views/company.py
@@ -1,4 +1,5 @@
 from json import dumps
+import json
 from uuid import UUID
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
@@ -11,23 +12,29 @@ def get_put_delete_profile(request: HttpRequest, id: UUID):
             return CompanyModel.objects.get(id=id)
         except (CompanyModel.DoesNotExist, CompanyModel.MultipleObjectsReturned):
             raise    
-    elif request.method == "PUT":
+    elif request.method == "POST":
         try:
-            # may be needed to implement with **kwargs
-            obj = CompanyModel.objects.create(*request.PUT.values())
-            obj.save()
-            return JsonResponse(obj)
+            data = request.POST.dict()
+            obj = CompanyModel.objects.get(id=data["id"])
+            {exec(f"obj.{attr}={val}") for attr,val in data.items()}
+            obj.save(update_fields=data.keys())
+            return JsonResponse("Successfully updated entry with id={id}")
         except Exception as e:
             raise
     elif request.method == "DELETE":
-        pass
+        try:
+            CompanyModel.objects.get(id=id).delete()
+            return JsonResponse("Successfully deleted entry with id = {id}")
+        except Exception as e:
+            raise
 
 
 def post_profile(request: HttpRequest):
     try:
-        # may be needed to implement with **kwargs
         data = request.POST.dict()
-        CompanyModel.objects.create(**data)
+        obj = CompanyModel(**data)
+        obj.save()
+        data["id"]=obj.id
         return JsonResponse(data,status=201)
     except Exception as e:
         raise
