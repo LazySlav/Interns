8fb9deb3b1d984726d7b0e6cc5033a4ab8ad26e0
LazySlav <72216859+LazySlav@users.noreply.github.com>
2023-07-20 23:18:49 +0300

    FINAL MODELS

--- SunSHINE/web/models.py
+++ SunSHINE/web/models.py
@@ -23,20 +23,25 @@ from django.contrib.auth.models import AbstractUser
 class ID(models.Model):
     id = models.IntegerField(
         primary_key=True, default=random.randint(100_000, 2_147_483_646), editable=False)
+
     class Meta:
         abstract = True
-#{"id":"440420080"}
+# {"id":"440420080"}
+
 
 class Human(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    patronymic = models.CharField(max_length=100,blank=True)
+    patronymic = models.CharField(max_length=100, blank=True)
     mail = models.EmailField(max_length=100)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone = models.CharField(validators=[phone_regex], max_length=17)
+
     class Meta:
         abstract = True
-#{"name":"ivan", "surname":"chelovekov", "patronymic":"petrovic", "mail":"test@mail.ru", "phone":"+79205262623"}
+# {"name":"ivan", "surname":"chelovekov", "patronymic":"petrovic", "mail":"test@mail.ru", "phone":"+79205262623"}
+
 
 class CompanyModel(ID):
     # login = models.OneToOneField(UserModel, on_delete=models.CASCADE)
@@ -44,24 +49,30 @@ class CompanyModel(ID):
     name = models.CharField(max_length=100)
     legal_address = models.CharField(max_length=200)
     physical_address = models.CharField(max_length=200)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone = models.CharField(validators=[phone_regex], max_length=17)
     mail = models.EmailField(max_length=100)
     description = models.CharField(max_length=500, blank=True)
-#{"name":"jest", "legal_address": "abb", "physical_address":"aabb", "phone":"+79205262623","mail":"test@mail.ru"}
+# {"name":"jest", "legal_address": "abb", "physical_address":"aabb", "phone":"+79205262623","mail":"test@mail.ru"}
+
 
 class CuratorModel(ID, Human):
     # login = models.OneToOneField(UserModel, on_delete=models.CASCADE)
     # password = models.OneToOneField(UserModel, on_delete=models.CASCADE)
-    university = models.ForeignKey("UniversityTable", on_delete=models.DO_NOTHING)
-    recommended = models.ForeignKey("RecommendedTable", on_delete=models.CASCADE, blank=True,null=True)
-#{"university":"MIIGAIK"}
+    university = models.ForeignKey(
+        "UniversityTable", on_delete=models.DO_NOTHING)
+    recommended = models.ForeignKey(
+        "RecommendedTable", on_delete=models.CASCADE, blank=True, null=True)
+# {"university":"MIIGAIK"}
+
 
 class MentorModel(ID, Human):
     # login = models.OneToOneField(UserModel, on_delete=models.CASCADE)
     # password = models.OneToOneField(UserModel, on_delete=models.CASCADE)
     curator = models.ForeignKey(CuratorModel, on_delete=models.DO_NOTHING)
-#{"curator":"594238759"}
+# {"curator":"594238759"}
+
 
 class StudentModel(ID, Human):
     # login = models.OneToOneField(UserModel, on_delete=models.CASCADE)
@@ -69,12 +80,14 @@ class StudentModel(ID, Human):
     mentor = models.ForeignKey(MentorModel, on_delete=models.DO_NOTHING)
     profession = models.CharField(max_length=30)
     resume = models.CharField(max_length=1000, blank=True)
-    active_vacancy = models.ForeignKey("VacancyModel", on_delete=models.DO_NOTHING, blank=True)
+    active_vacancy = models.ForeignKey(
+        "VacancyModel", on_delete=models.DO_NOTHING, blank=True)
     # __table_args__ = (
     #     models.Index('student_short', 'name', 'surname', 'patronymic',
     #                  'profession', 'mentor_id')
     # )
-#{"mentor":"447513381", profession":"programmer", "resume":"i can do something. hire me pls"}
+# {"mentor":"447513381", profession":"programmer", "resume":"i can do something. hire me pls"}
+
 
 class VacancyModel(ID):
     VACANCYSTATUS = [
@@ -92,7 +105,7 @@ class VacancyModel(ID):
     end_date = models.DateField(auto_now_add=True)
     address = models.CharField(max_length=250)
     description = models.CharField(max_length=250)
-#{"status":"checked", "tasks":"do something pls", "start_date":"15.07.2023", "end_date":"22.07.2023", "address":"aabb", "description":"do something pls"}
+# {"status":"checked", "tasks":"do something pls", "start_date":"15.07.2023", "end_date":"22.07.2023", "address":"aabb", "description":"do something pls"}
 
 
 # class TaskModel(ID):
@@ -123,17 +136,18 @@ class VacancyModel(ID):
 
 class UniversityTable(models.Model):
     university = models.CharField(max_length=100, primary_key=True)
-#{"university":"MIIGAIK"}
+# {"university":"MIIGAIK"}
+
 
 class WorkersTable(models.Model):
-    vacancy = models.ForeignKey("VacancyModel", primary_key=True, on_delete=models.CASCADE)
+    vacancy = models.ForeignKey(
+        "VacancyModel", primary_key=True, on_delete=models.CASCADE)
     profession = models.CharField(max_length=50)
     amount = models.IntegerField()
 
     def __str__(self):
         return f"{self.profession}: {self.amount}"
-#{"profession":"programmer", "amount":"5"}
-
+# {"profession":"programmer", "amount":"5"}
 
 
 class RecommendedTable(models.Model):
